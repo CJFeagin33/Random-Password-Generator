b4bb73bd1bea863830acb6e9687c9fb8acc72b72
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-06-29 01:11:14 -0700

    added logic regarding password length and content.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("How many characters long would you like your password to be?");
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Error: Passwords must be between 8 and 128 characters long.");
+    } else {
+      confirm("your password will be " + passwordLength + " characters long.");
+    }
+  
+  var lowerecase = prompt("Would you like your password to include lowercase characters?")
+    if (lowerecase === "yes") {
+      alert("Your password will contain lowercase characters.")
+    } else if (lowerecase === "no") {
+      alert("Your password will not contain any lowercase characters.")
+    } else {
+      alert("Error: answer was not a yes or no.")
+    }
+
+  var uppercase = prompt("Would you like your password to include uppercase characters?")
+    if (uppercase === "yes") {
+      alert("Your password will contain uppercase characters.")
+    } else if (uppercase === "no") {
+      alert("Your password will not contain any uppercase characters.")
+    } else {
+      alert("Error: answer was not a yes or no.")
+    }
+
+  var numeric = prompt("Would you like your password to include numeric characters?")
+    if (numeric === "yes") {
+      alert("Your password will contain numeric characters.")
+    } else if (numeric === "no") {
+      alert("Your password will not contain any numeric characters.")
+    } else {
+      alert("Error: answer was not a yes or no.")
+    }
+
+  var special = prompt("Would you like your password to include special characters? (ie. !%#$&)")
+    if (special === "yes") {
+      alert("Your password will contain special characters.")
+    } else if (special === "no") {
+      alert("Your password will not contain any special characters.")
+    } else {
+      alert("Error: answer was not a yes or no.")
+    }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
